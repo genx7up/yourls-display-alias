120b5e79c02faa4fa95ef775cd2ed4bf8e39b9c7
genx7up <genx7up@users.noreply.github.com>
2015-08-31 09:07:07 -0700

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -11,7 +11,7 @@
      
 yourls_add_filter( 'yourls_link', 'display_alias' );
 
-function display_alias () {
+function display_alias ($link, $keyword) {
   $link = YOURLS_ALIAS . '/' . yourls_sanitize_keyword( $keyword );
 	return $link;
 }
