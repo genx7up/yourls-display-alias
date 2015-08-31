e397679e557e196c8d9c6f217f5ed2448f6936c1
genx7up <genx7up@users.noreply.github.com>
2015-08-31 09:04:18 -0700

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+     Plugin Name: Display Alias
+     Plugin URI: https://github.com/genx7up/yourls-display-alias
+     Description: It enables to use a different alias domain for the shortned url
+     Version: 0.1
+     Author: G-Man
+     Author URI: https://github.com/genx7up/
+*/
+     
 yourls_add_filter( 'yourls_link', 'display_alias' );
 
 function display_alias () {
